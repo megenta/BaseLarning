a414e7c3173e39aedc86f4274bbd9c36d3b8e369
megenta <40060637+megenta@users.noreply.github.com>
2018-06-13 11:41:15 -0400

    Update and rename PairTotalsToNumver to PairTotalsToNumber.java

---
+++ PairTotalsToNumber.java
@@ -0,0 +1,13 @@
+
+// Given an array [1 ,3, 5 , -1 , 4 , 1]
+// Find any pairs which sum to 4
+// if duplicates?
+// if negative ?
+// is 1-3 & 3-1 considered seperate
+
+
+// a double for loop => O(n^2)
+
+// use a set so its O(n) + O(1)
+
+// use hash map if duplicates exist 
